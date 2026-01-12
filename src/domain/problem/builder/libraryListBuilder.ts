bf6ce8fb8134a351a220d5c7050cfb7dd61044ec
Your Name <you@example.com>
2026-01-12 13:35:11 +0900

    fix

--- src/domain/problem/builder/libraryListBuilder.ts
+++ src/domain/problem/builder/libraryListBuilder.ts
@@ -1,16 +1,12 @@
 import type { SortState } from "@/domain/problem/query/types/Sort"
 import { applySort } from "@/domain/problem/query/applySort"
-import type { LearningRecord } from '@/domain/learning/types'
-import type { Problem } from '@/domain/problem/Problem'
 import type { Exercise } from "@/domain/Exercise/Exercise"
 
 
 export const buildLibraryList = (
     exercises: Exercise[],
-    //problems: Problem[], 
     sort: SortState,
-    //learningRecords: LearningRecord,
 ) => {
     return applySort(exercises, sort)
-    //return applySort(problems, sort, learningRecords)    
+
 }
--- src/domain/problem/builder/queueBuilder.ts
+++ src/domain/problem/builder/queueBuilder.ts
@@ -1,7 +1,5 @@
 import type { Exercise } from "@/domain/Exercise/Exercise"
 import type { QueueItem } from "@/domain/fsm/types"
-import type { Problem } from '@/domain/problem/Problem'
-
 
 export const buildQueue = (exercises: Exercise[]): QueueItem[] => {       
     return exercises.map((e) => ({problemId: e.problem.id}))
